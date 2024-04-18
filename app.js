1522b5e82fe6bf8fa3cbe7555ebfa743c599acb7
LuvArora443 <luvarora2003@gmail.com>
2024-04-18 22:07:36 +0530

    logout

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const authRoutes = require("./routes/authRoutes");
-// const cookieParser = require('cookie-parser');
-
-// const app = express();
-
-// // middleware
-// app.use(express.static('public'));
-// app.use(express.json());
-// app.use(cookieParser);
-
-// // view engine
-// app.set('view engine', 'ejs');
-
-// // database connection
-// const dbURI = 'mongodb+srv://luvarora2003:luvaroranec2003@cluster0.rvtyzge.mongodb.net/node_auth';
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => app.listen(4000))
-//   .catch((err) => console.log(err));
-
-// // routes
-// app.get('/', (req, res) => res.render('home'));
-// app.use(authRoutes);
-
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cookieParser = require('cookie-parser');
+const { requireAuth } = require('./middleware/authMiddleware');
 
 const app = express();
 
@@ -46,4 +22,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // routes
 app.get('/', (req, res) => res.render('home'));
+app.get('/home2', requireAuth, (req, res) => res.render('home2'));
 app.use(authRoutes);
