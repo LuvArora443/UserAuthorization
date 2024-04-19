3b22ac69082ae4e631961eb9c98e8823da8a6317
Luv Arora <139987286+LuvArora443@users.noreply.github.com>
2024-04-20 01:51:06 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,8 +9,13 @@ router.get('/signup', authController.signup_get);
 router.post('/signup', authController.signup_post);
 router.get('/login', authController.login_get);
 router.post('/login', authController.login_post);
+router.get('/login2', authController.login2_get);
+router.post('/login2', authController.login2_post);
 router.get('/logout', authController.logout_get);
 router.get('/user-info', requireAuth, authController.getUserInfo);
+router.get('/details',requireAuth,authController.details_get)
+router.post('/details', requireAuth, authController.details_post);
+
 
 
 module.exports = router;
